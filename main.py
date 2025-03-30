84804ef84eb5d1bc7cc8c06125e3563f0f587487
JDH1987 <johnhorton187@gmail.com>
2025-03-30 15:50:50 -0700

    Drew a player triangle

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
 
@@ -10,6 +11,9 @@ def main():
 
 	clock = pygame.time.Clock()
 	dt = 0
+
+	# spawn player in middle of the screen
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -19,10 +23,16 @@ def main():
 			if event.type == pygame.QUIT:
 				return
 
+		# fill the screen with clack
 		screen.fill((0, 0, 0))
 
+		# draw the player
+		player.draw(screen)
+
+		# flip the screen
 		pygame.display.flip()
 
+		# cap the frame rate
 		dt = clock.tick(60) / 1000
 
 
